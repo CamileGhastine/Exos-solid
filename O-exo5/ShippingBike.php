65cbd42d4428078de7b282273b6ecb4b9abf587e
CamileGhastine <camileghastine@hotmail.com>
2021-09-07 15:15:17 +0200

    exo4 :refactor

--- O-exo5/ShippingBike.php
+++ O-exo5/ShippingBike.php
@@ -2,8 +2,6 @@
 
 class ShippingBike extends Shipping
 {
-    private float $cost;
-
     public function getCost(Order $order): float
     {
         return $order->getWeight() < 10 ? 10 : $order->getWeight();
--- O-exo5/ShippingFeet.php
+++ O-exo5/ShippingFeet.php
@@ -2,8 +2,6 @@
 
 class ShippingFeet extends Shipping
 {
-    private float $cost;
-
     public function getCost(Order $order): float
     {
         return $order->getWeight() < 10 ? 5 : $order->getWeight() / 2;
